2de3b63af1f2dc9c107a74283ec8fbc2627d9da6
lDeNl <87079728+lDeNl@users.noreply.github.com>
2022-10-13 16:10:09 +0300

    v2.1

--- class-astro-public.php
+++ class-astro-public.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The public-facing functionality of the plugin.
  *
@@ -9,136 +8,7 @@
  * @package    Astro
  * @subpackage Astro/public
  */
-
-/**
- * The public-facing functionality of the plugin.
- *
- * Defines the plugin name, version, and two examples hooks for how to
- * enqueue the public-facing stylesheet and JavaScript.
- *
- * @package    Astro
- * @subpackage Astro/public
- * @author     Vakhtang Avaliani <vavaliani@gmail.com>
- */
 class Astro_Public {
-
-
-        public $timezones = array(
-        'Pacific/Midway' => "(GMT-11:00) Midway Island",
-        'US/Samoa' => "(GMT-11:00) Samoa",
-        'US/Hawaii' => "(GMT-10:00) Hawaii",
-        'US/Alaska' => "(GMT-09:00) Alaska",
-        'US/Pacific' => "(GMT-08:00) Pacific Time (US &amp; Canada)",
-        'America/Tijuana' => "(GMT-08:00) Tijuana",
-        'US/Arizona' => "(GMT-07:00) Arizona",
-        'US/Mountain' => "(GMT-07:00) Mountain Time (US &amp; Canada)",
-        'America/Chihuahua' => "(GMT-07:00) Chihuahua",
-        'America/Mazatlan' => "(GMT-07:00) Mazatlan",
-        'America/Mexico_City' => "(GMT-06:00) Mexico City",
-        'America/Monterrey' => "(GMT-06:00) Monterrey",
-        'Canada/Saskatchewan' => "(GMT-06:00) Saskatchewan",
-        'US/Central' => "(GMT-06:00) Central Time (US &amp; Canada)",
-        'US/Eastern' => "(GMT-05:00) Eastern Time (US &amp; Canada)",
-        'US/East-Indiana' => "(GMT-05:00) Indiana (East)",
-        'America/Bogota' => "(GMT-05:00) Bogota",
-        'America/Lima' => "(GMT-05:00) Lima",
-        'America/Caracas' => "(GMT-04:30) Caracas",
-        'Canada/Atlantic' => "(GMT-04:00) Atlantic Time (Canada)",
-        'America/La_Paz' => "(GMT-04:00) La Paz",
-        'America/Santiago' => "(GMT-04:00) Santiago",
-        'Canada/Newfoundland' => "(GMT-03:30) Newfoundland",
-        'America/Buenos_Aires' => "(GMT-03:00) Buenos Aires",
-        'Greenland' => "(GMT-03:00) Greenland",
-        'Atlantic/Stanley' => "(GMT-02:00) Stanley",
-        'Atlantic/Azores' => "(GMT-01:00) Azores",
-        'Atlantic/Cape_Verde' => "(GMT-01:00) Cape Verde Is.",
-        'Africa/Casablanca' => "(GMT) Casablanca",
-        'Europe/Dublin' => "(GMT) Dublin",
-        'Europe/Lisbon' => "(GMT) Lisbon",
-        'Europe/London' => "(GMT) London",
-        'Africa/Monrovia' => "(GMT) Monrovia",
-        'Europe/Amsterdam' => "(GMT+01:00) Amsterdam",
-        'Europe/Belgrade' => "(GMT+01:00) Belgrade",
-        'Europe/Berlin' => "(GMT+01:00) Berlin",
-        'Europe/Bratislava' => "(GMT+01:00) Bratislava",
-        'Europe/Brussels' => "(GMT+01:00) Brussels",
-        'Europe/Budapest' => "(GMT+01:00) Budapest",
-        'Europe/Copenhagen' => "(GMT+01:00) Copenhagen",
-        'Europe/Ljubljana' => "(GMT+01:00) Ljubljana",
-        'Europe/Madrid' => "(GMT+01:00) Madrid",
-        'Europe/Paris' => "(GMT+01:00) Paris",
-        'Europe/Prague' => "(GMT+01:00) Prague",
-        'Europe/Rome' => "(GMT+01:00) Rome",
-        'Europe/Sarajevo' => "(GMT+01:00) Sarajevo",
-        'Europe/Skopje' => "(GMT+01:00) Skopje",
-        'Europe/Stockholm' => "(GMT+01:00) Stockholm",
-        'Europe/Vienna' => "(GMT+01:00) Vienna",
-        'Europe/Warsaw' => "(GMT+01:00) Warsaw",
-        'Europe/Zagreb' => "(GMT+01:00) Zagreb",
-        'Europe/Athens' => "(GMT+02:00) Athens",
-        'Europe/Bucharest' => "(GMT+02:00) Bucharest",
-        'Africa/Cairo' => "(GMT+02:00) Cairo",
-        'Africa/Harare' => "(GMT+02:00) Harare",
-        'Europe/Helsinki' => "(GMT+02:00) Helsinki",
-        'Europe/Istanbul' => "(GMT+02:00) Istanbul",
-        'Asia/Jerusalem' => "(GMT+02:00) Jerusalem",
-        'Europe/Kiev' => "(GMT+02:00) Kyiv",
-        'Europe/Minsk' => "(GMT+02:00) Minsk",
-        'Europe/Riga' => "(GMT+02:00) Riga",
-        'Europe/Sofia' => "(GMT+02:00) Sofia",
-        'Europe/Tallinn' => "(GMT+02:00) Tallinn",
-        'Europe/Vilnius' => "(GMT+02:00) Vilnius",
-        'Asia/Baghdad' => "(GMT+03:00) Baghdad",
-        'Asia/Kuwait' => "(GMT+03:00) Kuwait",
-        'Africa/Nairobi' => "(GMT+03:00) Nairobi",
-        'Asia/Riyadh' => "(GMT+03:00) Riyadh",
-        'Europe/Moscow' => "(GMT+03:00) Moscow",
-        'Asia/Tehran' => "(GMT+03:30) Tehran",
-        'Asia/Baku' => "(GMT+04:00) Baku",
-        'Europe/Volgograd' => "(GMT+04:00) Volgograd",
-        'Asia/Muscat' => "(GMT+04:00) Muscat",
-        'Asia/Tbilisi' => "(GMT+04:00) Tbilisi",
-        'Asia/Yerevan' => "(GMT+04:00) Yerevan",
-        'Asia/Kabul' => "(GMT+04:30) Kabul",
-        'Asia/Karachi' => "(GMT+05:00) Karachi",
-        'Asia/Tashkent' => "(GMT+05:00) Tashkent",
-        'Asia/Kolkata' => "(GMT+05:30) Kolkata",
-        'Asia/Kathmandu' => "(GMT+05:45) Kathmandu",
-        'Asia/Yekaterinburg' => "(GMT+06:00) Ekaterinburg",
-        'Asia/Almaty' => "(GMT+06:00) Almaty",
-        'Asia/Dhaka' => "(GMT+06:00) Dhaka",
-        'Asia/Novosibirsk' => "(GMT+07:00) Novosibirsk",
-        'Asia/Bangkok' => "(GMT+07:00) Bangkok",
-        'Asia/Jakarta' => "(GMT+07:00) Jakarta",
-        'Asia/Krasnoyarsk' => "(GMT+08:00) Krasnoyarsk",
-        'Asia/Chongqing' => "(GMT+08:00) Chongqing",
-        'Asia/Hong_Kong' => "(GMT+08:00) Hong Kong",
-        'Asia/Kuala_Lumpur' => "(GMT+08:00) Kuala Lumpur",
-        'Australia/Perth' => "(GMT+08:00) Perth",
-        'Asia/Singapore' => "(GMT+08:00) Singapore",
-        'Asia/Taipei' => "(GMT+08:00) Taipei",
-        'Asia/Ulaanbaatar' => "(GMT+08:00) Ulaan Bataar",
-        'Asia/Urumqi' => "(GMT+08:00) Urumqi",
-        'Asia/Irkutsk' => "(GMT+09:00) Irkutsk",
-        'Asia/Seoul' => "(GMT+09:00) Seoul",
-        'Asia/Tokyo' => "(GMT+09:00) Tokyo",
-        'Australia/Adelaide' => "(GMT+09:30) Adelaide",
-        'Australia/Darwin' => "(GMT+09:30) Darwin",
-        'Asia/Yakutsk' => "(GMT+10:00) Yakutsk",
-        'Australia/Brisbane' => "(GMT+10:00) Brisbane",
-        'Australia/Canberra' => "(GMT+10:00) Canberra",
-        'Pacific/Guam' => "(GMT+10:00) Guam",
-        'Australia/Hobart' => "(GMT+10:00) Hobart",
-        'Australia/Melbourne' => "(GMT+10:00) Melbourne",
-        'Pacific/Port_Moresby' => "(GMT+10:00) Port Moresby",
-        'Australia/Sydney' => "(GMT+10:00) Sydney",
-        'Asia/Vladivostok' => "(GMT+11:00) Vladivostok",
-        'Asia/Magadan' => "(GMT+12:00) Magadan",
-        'Pacific/Auckland' => "(GMT+12:00) Auckland",
-        'Pacific/Fiji' => "(GMT+12:00) Fiji",
-        );
-
-
 	/**
 	 * The ID of this plugin.
 	 *
@@ -147,7 +17,6 @@ class Astro_Public {
 	 * @var      string    $plugin_name    The ID of this plugin.
 	 */
 	private $plugin_name;
-
 	/**
 	 * The version of this plugin.
 	 *
@@ -156,7 +25,6 @@ class Astro_Public {
 	 * @var      string    $version    The current version of this plugin.
 	 */
 	private $version;
-
 	/**
 	 * Initialize the class and set its properties.
 	 *
@@ -168,7 +36,6 @@ class Astro_Public {
 
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
-
 	}
 
 	/**
@@ -191,21 +58,14 @@ class Astro_Public {
 		 */
         $options = get_option('astro-option-settings');
         $options = !empty($options) ? json_decode($options, true) : '';
-
-
         if (is_page($options['exclude_style'])) {
             wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/astro-public.css', array(), $this->version, 'all' );
             wp_enqueue_style( $this->plugin_name . '_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css', array(), $this->version, 'all' );
             wp_enqueue_style( $this->plugin_name . '_datatables', '//cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css', array(), $this->version, 'all' );
             wp_enqueue_style( $this->plugin_name . '_jqueryui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css', array(), $this->version, 'all' );
-
             wp_enqueue_style( $this->plugin_name . '_assests', plugin_dir_url( __FILE__ ) . 'assets/css/style.css', array(), $this->version, 'all' );
         }
-
-
 	}
-
-
     // wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' );
     //}
 
@@ -231,24 +91,14 @@ class Astro_Public {
 		 * class.
 		 */
         if (is_page($options['exclude_style'])) {
-
             wp_enqueue_script( $this->plugin_name . '_astro_jquery', plugin_dir_url( __FILE__ ) . 'assets/js/jquery.min.js', array( '' ), $this->version, true );
             wp_enqueue_script( $this->plugin_name . '_astro_bootstrap', plugin_dir_url( __FILE__ ) . 'assets/js/bootstrap.min.js', array( 'jquery' ), $this->version, true );
             wp_enqueue_script( $this->plugin_name . '_astro_jqueryui_js', plugin_dir_url( __FILE__ ) . 'assets/js/jquery-ui.min.js', array( 'jquery' ), $this->version, true );
             wp_enqueue_script( $this->plugin_name . '_astro_inputmask', plugin_dir_url( __FILE__ ) . 'assets/js/jquery.inputmask.js', array( 'jquery' ), $this->version, true );
             wp_enqueue_script( $this->plugin_name . '_astro_dataTables', '//cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js', array( 'jquery' ), $this->version, true );
-
             wp_enqueue_script( $this->plugin_name . '_app', plugin_dir_url( __FILE__ ) . 'assets/js/app.js', array( 'jquery' ), $this->version, true );
             wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/astro-public.js', array( 'jquery' ), $this->version, true );
         }
-
-
-
         wp_localize_script($this->plugin_name, "ajax_obj", array('ajaxurl' => admin_url( 'admin-ajax.php' )));
-
 	}
-
-
-
-
 }
